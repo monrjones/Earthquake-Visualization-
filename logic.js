12d1022c9c36f6f9cc8799e9302788ecb5fef990
Monica Jones <monrjones@gmail.com>
2019-04-02 19:59:44 -0400

    update

--- logic.js
+++ logic.js
@@ -4,7 +4,26 @@ var earthquake_link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary
 // Store our API endpoint inside queryUrl
 //var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
   //"2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
+  function markerSize(mag) {
+    return mag * 30000;
+  }
+  
+  function markerColor(mag) {
+    if (mag <= 1) {
+        return "#ADFF2F";
+    } else if (mag <= 2) {
+        return "#9ACD32";
+    } else if (mag <= 3) {
+        return "#FFFF00";
+    } else if (mag <= 4) {
+        return "#ffd700";
+    } else if (mag <= 5) {
+        return "#FFA500";
+    } else {
+        return "#FF0000";
+    };
+  }
+  
 // Perform a GET request to the query URL
 d3.json(earthquake_link, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
@@ -39,7 +58,7 @@ function createFeatures(earthquakeData) {
    
   // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
-}
+}; 
 function createMap(earthquakes) {
   // Define satelitemap and darkmap layers
   var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -57,13 +76,13 @@ function createMap(earthquakes) {
   });
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Street Map": satelitemap,
+    "Sat Map": satelitemap,
     "Dark Map": darkmap
   };
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes,
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -81,22 +100,25 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  //legend
   var legend = L.control({position: 'bottomright'});
 
-  legend.onAdd = function () {
-  
-      var div = L.DomUtil.create('div', 'info legend'),
-          magnitudes = [0, 1, 2, 3, 4, 5];
-  
-      for (var i = 0; i < magnitudes.length; i++) {
-          div.innerHTML +=
-              '<i style="background:' + markerColor(magnitudes[i] + 1) + '"></i> ' + 
-      + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
+  legend.onAdd = function(map) {
+
+    var div = L.DomUtil.create('div','info legend'),
+        magnitudes = [0,1,2,3,4,5],
+        labels = [];
+
+    div.innerHTML += "<h4 style='margin:4px'>Magnitude</h4>" 
+    // loop through our density intervals and generate a label for each interval
+    for (var i=0; i < magnitudes.length; i++){
+      div.innerHTML +=
+        '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
+        magnitudes[i] + (magnitudes[i+1]?'&ndash;' + magnitudes[i+1] +'<br>': '+');
       }
-  
       return div;
   };
-  
   legend.addTo(myMap);
 
 
